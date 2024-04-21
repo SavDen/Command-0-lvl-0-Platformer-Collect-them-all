e6c73283e89e4b524c851040349668e3c2680995
Andrei Huk <andreihukgamedev@gmail.com>
2024-04-21 04:58:05 +0200

    FINAL REVIEW
    Changed all bugs

--- Assets/Scripts V02/TriggerRespown.cs
+++ Assets/Scripts V02/TriggerRespown.cs
@@ -1,12 +1,20 @@
+using System;
 using UnityEngine;
 
 public class TriggerRespown : MonoBehaviour
 {
+    [SerializeField] private GameObject _saveUI;
     private void OnTriggerEnter(Collider other)
     {
         if(other.TryGetComponent(out Move playerRespown))
         {
             playerRespown._respown = transform;
+            _saveUI.SetActive(true);
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _saveUI.SetActive(false);
     }
 }
